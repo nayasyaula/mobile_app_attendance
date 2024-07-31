cd3aa5a126c71f841983c4c2e41dbf0befaa8e97
nayasyaula <[200~nayasyaulamawarni@smkwikrama.sch.idgit config --global user.email nayasyaulamawarni@smkwikrama.sch.id>
2024-07-31 10:58:43 +0700

    register

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { UserProvider } from './components/UserContext';
 
 import DashboardScreen from './components/DashboardScreen';
 import LandingScreen from './components/LandingScreen';
@@ -26,28 +27,30 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
-        {/* <Stack.Screen name="Dashboard" component={DashboardScreen} /> */}
-        <Stack.Screen name="Landing" component={LandingScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ToDoList" component={ToDoListScreen} />
-        <Stack.Screen name="CreateToDoList" component={CreateToDoListScreen} />
-        <Stack.Screen name="DetailToDoList" component={DetailToDoList} />
-        <Stack.Screen name="UpdateToDoList" component={UpdateToDoList} />
-        <Stack.Screen name='ScanScreen' component={ScanScreen} />
-        <Stack.Screen name='AttendanceScreen' component={AttendanceScreen} />
-        {/* <Stack.Screen name="SuccessAttendanceIn" component={SuccessAttendanceInScreen} /> */}
-        {/* <Stack.Screen name="SuccessAttendanceLate" component={SuccessAttendanceLate} /> */}
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="AboutAcc" component={AboutAccountScreen} />
-        <Stack.Screen name="Password1" component={PasswordOneScreen} />
-        <Stack.Screen name="Password2" component={PasswordTwoScreen} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SplashScreen">
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          {/* <Stack.Screen name="Dashboard" component={DashboardScreen} /> */}
+          <Stack.Screen name="Landing" component={LandingScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ToDoList" component={ToDoListScreen} />
+          <Stack.Screen name="CreateToDoList" component={CreateToDoListScreen} />
+          <Stack.Screen name="DetailToDoList" component={DetailToDoList} />
+          <Stack.Screen name="UpdateToDoList" component={UpdateToDoList} />
+          <Stack.Screen name='ScanScreen' component={ScanScreen} />
+          <Stack.Screen name='AttendanceScreen' component={AttendanceScreen} />
+          {/* <Stack.Screen name="SuccessAttendanceIn" component={SuccessAttendanceInScreen} /> */}
+          {/* <Stack.Screen name="SuccessAttendanceLate" component={SuccessAttendanceLate} /> */}
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="AboutAcc" component={AboutAccountScreen} />
+          <Stack.Screen name="Password1" component={PasswordOneScreen} />
+          <Stack.Screen name="Password2" component={PasswordTwoScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </UserProvider>
   );
 };
 
